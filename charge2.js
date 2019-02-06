38f6eb58ea320d20a39995ce2dda578e94f41a59
Carrotlord <j.oliverchu@gmail.com>
2019-02-06 03:12:39 -0800

    Refactor player functionality.

--- charge2.js
+++ charge2.js
@@ -1,11 +1,7 @@
 var g = {
     context: null,
-    player: {
-        x: 200,
-        y: 200,
-        walkXVel: 5,
-        walkYVel: 5
-    }
+    player: new Player(),
+    entities: []
 };
 
 var SCREEN_HEIGHT = 500;
@@ -20,16 +16,16 @@ var KEY_RIGHT = 39;
 function keyDownAction(event) {
     switch (event.keyCode) {
         case KEY_DOWN:
-            g.player.y += g.player.walkYVel;
+            g.player.walkDown();
             break;
         case KEY_UP:
-            g.player.y -= g.player.walkYVel;
+            g.player.walkUp();
             break;
         case KEY_LEFT:
-            g.player.x -= g.player.walkXVel;
+            g.player.walkLeft();
             break;
         case KEY_RIGHT:
-            g.player.x += g.player.walkXVel;
+            g.player.walkRight();
             break;
     }
 }
@@ -56,10 +52,6 @@ function drawRect(x, y, width, height) {
     g.context.fill();
 }
 
-function drawPlayer() {
-    drawCircle(g.player.x, g.player.y, 10);
-}
-
 function clearScreen() {
     g.context.fillStyle = GAME_BG_COLOR;
     g.context.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -69,11 +61,14 @@ function draw() {
     window.requestAnimationFrame(draw);
     clearScreen();
     drawRect(20, 20, 40, 50);
-    drawPlayer();
+    for (var i = 0; i < g.entities.length; i++) {
+        g.entities[i].draw();
+    }
     drawRect(100, 20, 40, 50);
 }
 
 function setup() {
+    g.entities.push(g.player);
     var canvas = document.getElementById("screen");
     canvas.height = SCREEN_HEIGHT;
     canvas.width = SCREEN_WIDTH;
---
+++ player.js
@@ -0,0 +1,26 @@
+function Player() {
+    this.x = 200;
+    this.y = 200;
+    this.walkXVel = 5;
+    this.walkYVel = 5;
+}
+
+Player.prototype.walkDown = function() {
+    this.y += this.walkYVel;
+}
+
+Player.prototype.walkUp = function() {
+    this.y -= this.walkYVel;
+}
+
+Player.prototype.walkLeft = function() {
+    this.x -= this.walkXVel;
+}
+
+Player.prototype.walkRight = function() {
+    this.x += this.walkXVel;
+}
+
+Player.prototype.draw = function() {
+    drawCircle(this.x, this.y, 10);
+}
